e3d2fa3caac46181c7c76ec5b50e05f1a31305e2
Данила Горнушко <gornushko@naci.local>
2025-02-14 14:43:59 +0300

    hello world both via uart and defmt

--- justfile
+++ justfile
@@ -1,4 +1,6 @@
-flash-size:
+size:
 	cargo build --release
 	cargo size --release -- -A | awk '/\.vector_table/ { v=$2 } /\.text/ { t=$2 } /\.rodata/ { r=$2 } END {print "FLASH SIZE used:" v+t+r}'
 
+run:
+	cargo run --release
--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -6,7 +6,10 @@ use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
+use embassy_stm32::usart::{Uart, Config};
+use embassy_stm32::{bind_interrupts, peripherals, usart};
 use defmt::info;
+use embedded_io::Write;
 
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
@@ -15,13 +18,20 @@ use embassy_time::{Duration, Timer};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    // let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+    // #[cfg(feature = "log")]
+    let mut uart_config = Config::default();
+    uart_config.baudrate = 57600;
+    let mut usart = Uart::new_blocking_with_de(
+        p.LPUART1,
+        p.PA3,
+        p.PA2,
+        p.PB1,
+        uart_config
+    ).unwrap();
 
     loop {
         info!("Hello, World!");
-        // led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-        // led.set_low();
+        usart.write_all(b"Hello, World!\n").unwrap();
         Timer::after(Duration::from_millis(500)).await;
     }
 }
