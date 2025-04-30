dacb61b480327f7f84a1da02a019d8a9560ef853
okhsunrog <me@okhsunrog.dev>
2025-04-30 14:35:49 +0300

    added uart, more defmt features

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,18 +1,27 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+use defmt::{info, unwrap};
 use embassy_stm32::{
+    bind_interrupts,
     gpio::{Level, Output, Speed},
+    peripherals,
     rcc::{Hse, HseMode, LsConfig, Sysclk},
     time::mhz,
+    usart::{self, BufferedUart, Config},
 };
 use embassy_time::{Duration, Timer};
+use embedded_io_async::Write;
 use panic_abort as _;
 use rtt_target::{ChannelMode::NoBlockSkip, rtt_init_defmt};
 
+bind_interrupts!(struct Irqs {
+    LPUART1 => usart::BufferedInterruptHandler<peripherals::LPUART1>;
+});
+
 #[embassy_executor::main]
 async fn main(_spawner: embassy_executor::Spawner) {
+    rtt_init_defmt!(NoBlockSkip, 512);
     let mut config = embassy_stm32::Config::default();
     {
         config.rcc.ls = LsConfig::off();
@@ -24,16 +33,32 @@ async fn main(_spawner: embassy_executor::Spawner) {
         config.rcc.sys = Sysclk::HSE;
     }
     let p = embassy_stm32::init(config);
-    rtt_init_defmt!(NoBlockSkip, 512);
 
     let mut led1 = Output::new(p.PA7, Level::High, Speed::Low);
     let mut led2 = Output::new(p.PA6, Level::Low, Speed::Low);
+
+    let mut uart_config = Config::default();
+    uart_config.baudrate = 57600;
+    let mut tx_buf = [0u8; 256];
+    let mut rx_buf = [0u8; 256];
+
+    let mut usart = unwrap!(BufferedUart::new_with_de(
+        p.LPUART1,
+        p.PA3, // RX
+        p.PA2, // TX
+        p.PB1, // DE
+        Irqs,
+        &mut tx_buf,
+        &mut rx_buf,
+        uart_config,
+    ));
 
     // blink
     loop {
         led1.toggle();
         led2.toggle();
         info!("Hello, world!");
+        unwrap!(usart.write_all(b"Hello, world!\r\n").await);
         Timer::after(Duration::from_secs(1)).await;
     }
 }
